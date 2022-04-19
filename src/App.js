57ab2704e55fc55ed83759732352aed641d70d74
Kaden Hansen <kadendeanhansen@gmail.com>
2022-04-18 21:19:11 -0400

    Add useEffect connection to server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+
+
 function App() {
+
+  useEffect(() => {
+  const callBackendAPI = async () => {
+    const response = await fetch('http://localhost:5000/');
+    const body = await response.json();
+    console.log(body)
+  };  
+  callBackendAPI()
+}, [])
+
   return (
     <div>
+      Ibonsai
     </div>
   );
 }
