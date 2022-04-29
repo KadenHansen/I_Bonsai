ea5595fcae886d597540dbac2eeebdd57977d70f
Kaden Hansen <kadendeanhansen@gmail.com>
2022-04-29 17:32:29 -0400

    replace substitute image in tree view page with tree image data

--- src/Components/TreeView/Profile.js
+++ src/Components/TreeView/Profile.js
@@ -11,7 +11,7 @@ const Profile = (props) => {
         className='rounded mt-5'
         width='150px'
         alt='Your Bonsai Tree'
-        src='https://www.thespruce.com/thmb/0AJv8AfVaT09o4oLhofX_WR_d-Y=/3024x3022/filters:no_upscale():max_bytes(150000):strip_icc()/Bonsai-Juniper-Tree_GettyImages-1191078639-c86fffdd513648b6b2ee0d658710bc7d.jpg'
+        src={props.data.Image}
       />
       <div className='font-weight-bold'>{props.data.Name}</div>
       <div className='text-black-50'>{props.data.Species}</div>
