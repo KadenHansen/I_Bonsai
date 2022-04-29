f09bb29084998c4ae8ba6f16f811e6e2002e7607
Kaden Hansen <kadendeanhansen@gmail.com>
2022-04-29 18:24:30 -0400

    clean profile component

--- src/Components/TreeView/Profile.js
+++ src/Components/TreeView/Profile.js
@@ -1,14 +1,6 @@
-import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
-import { Link } from 'react-router-dom'
-import EditTreeForm from '../EditTreeForm'
 
 const Profile = (props) => {
-  async function deleteTree(treeId) {
-    await fetch(`http://localhost:5000/Inventory/${treeId}/delete`, {
-      method: "DELETE"
-    })
-  }
 
   return (
     <Container>
@@ -20,13 +12,7 @@ const Profile = (props) => {
       />
       <div className='font-weight-bold h3'>{props.data.Name}</div>
       <div className='text-black-50'>{props.data.Species}</div>
-      {/* <EditTreeForm /> */}
-      {/* <Link to='edit'>
-        <Button variant='primary'>Edit Tree</Button>
-      </Link> */}
-      <Link to='/Inventory'>
-      <Button variant='danger' onClick={()=>{deleteTree(props.data._id)}}>Delete Tree</Button>
-      </Link>
+
     </Container>
   )
 }
